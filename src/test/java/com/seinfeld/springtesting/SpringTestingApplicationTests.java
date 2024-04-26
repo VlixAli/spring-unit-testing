5eaea1921cda1e96ef01532f05626b343d3ab810
Ali <qali372@gmail.com>
2024-04-26 19:20:46 +0300

    build : SpringTestingApplication.java to try simple tests

---
+++ src/test/java/com/seinfeld/springtesting/SpringTestingApplicationTests.java
@@ -0,0 +1,30 @@
+package com.seinfeld.springtesting;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SpringTestingApplicationTests {
+
+    Calculator underTest = new Calculator();
+
+    @Test
+    void itShouldAddTwoNumbers() {
+        // given
+        int numberOne = 20;
+        int numberTwo = 30;
+
+        // when
+        int result = underTest.add(numberOne, numberTwo);
+
+        // then
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
+
+}
